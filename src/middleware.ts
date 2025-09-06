747a35322fb051f3690da493f144b90572fcb72a
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2025-09-06 10:30:36 +0530

    shad and clerk

---
+++ src/middleware.ts
@@ -0,0 +1,37 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+
+export default clerkMiddleware(async (auth, req) => {
+    const { userId } = await auth();
+    const currentUrl = new URL(req.url);
+    const isAccessingDashboard = currentUrl.pathname === '/home';
+    const isApiReq = currentUrl.pathname.startsWith('/api');
+
+    if (userId && isPublicRoute(req) && !isAccessingDashboard) {
+        return NextResponse.redirect(new URL('/home', req.url));
+    }
+
+    //
+    if (!userId) {
+        if (!isPublicAPIRoute(req) && isPublicRoute(req))
+            return NextResponse.redirect(new URL('/signin', req.url));
+
+        if (isApiReq && !isPublicAPIRoute(req))
+            return NextResponse.redirect(new URL('/signin', req.url));
+    }
+
+    return NextResponse.next();
+});
+
+const isPublicRoute = createRouteMatcher(['/signin', '/signup', '/', '/home']);
+
+const isPublicAPIRoute = createRouteMatcher(['/api/videos']);
+
+export const config = {
+    matcher: [
+        // Skip Next.js internals and all static files, unless found in search params
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        // Always run for API routes
+        '/(api|trpc)(.*)',
+    ],
+};
